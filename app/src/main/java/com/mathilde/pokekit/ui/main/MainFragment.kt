ec79fa676b017106c6a333a3109951234114fe88
Mathilde <mathilde.guillossou@gmail.com>
2018-10-04 13:46:36 +0200

    configure local model and setup input and output data format

--- app/src/main/java/com/mathilde/pokekit/ui/main/MainFragment.kt
+++ app/src/main/java/com/mathilde/pokekit/ui/main/MainFragment.kt
@@ -1,12 +1,23 @@
 package com.mathilde.pokekit.ui.main
 
 import android.arch.lifecycle.ViewModelProviders
+import android.os.Build
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.firebase.ml.custom.FirebaseModelDataType
+import com.google.firebase.ml.custom.FirebaseModelInputOutputOptions
+import com.google.firebase.ml.custom.FirebaseModelInterpreter
+import com.google.firebase.ml.custom.FirebaseModelManager
+import com.google.firebase.ml.custom.FirebaseModelOptions
+import com.google.firebase.ml.custom.model.FirebaseCloudModelSource
+import com.google.firebase.ml.custom.model.FirebaseLocalModelSource
 import com.mathilde.pokekit.R
+import com.google.firebase.ml.custom.model.FirebaseModelDownloadConditions
+
+
 
 class MainFragment : Fragment() {
 
@@ -27,4 +38,49 @@ class MainFragment : Fragment() {
         // TODO: Use the ViewModel
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        loadModel()
+    }
+
+    fun loadModel() {
+        var conditionsBuilder = FirebaseModelDownloadConditions.Builder().requireWifi()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            // Enable advanced conditions on Android Nougat and newer.
+            conditionsBuilder = conditionsBuilder.requireCharging().requireDeviceIdle()
+        }
+
+
+        val conditions = conditionsBuilder.build() as FirebaseModelDownloadConditions
+        // Build a FirebaseCloudModelSource object by specifying the name you assigned the model
+        // when you uploaded it in the Firebase console.
+
+        val cloudSource = FirebaseCloudModelSource.Builder("poke-detector")
+                .enableModelUpdates(true)
+                .setInitialDownloadConditions(conditions)
+                .setUpdatesDownloadConditions(conditions)
+                .build()
+        FirebaseModelManager.getInstance().registerCloudModelSource(cloudSource)
+
+        //Load a local model using the FirebaseLocalModelSource Builder class
+        val fireBaseLocalModelSource = FirebaseLocalModelSource.Builder("poke-detector")
+                .setAssetFilePath("pokedex.tflite") //name of the .tflite model stored in asset folder
+                .build()
+
+        //Registering the model loaded above with the ModelManager Singleton
+        FirebaseModelManager.getInstance().registerLocalModelSource(fireBaseLocalModelSource)
+
+        val options =  FirebaseModelOptions.Builder()
+                .setCloudModelName("poke-detector")
+                .setLocalModelName("pokedex")
+                .build()
+
+        val firebaseInterpreter =
+                FirebaseModelInterpreter.getInstance(options)!!
+
+        val inputOutputOptions = FirebaseModelInputOutputOptions.Builder()
+                .setInputFormat(0, FirebaseModelDataType.FLOAT32, intArrayOf(1, 224, 224, 3))
+                .setOutputFormat(0, FirebaseModelDataType.FLOAT32, intArrayOf(1, 149))
+                .build()
+    }
 }
